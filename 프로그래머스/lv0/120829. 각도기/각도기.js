2cf2a2907fd80177010dd088adfdc30e74b44118
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-03-13 19:03:21 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -0,0 +1,12 @@
+function solution(angle) {
+    var answer = 0;
+    
+        if(angle<90) return answer = 1;
+        if(angle == 90) return answer = 2;
+        if(angle<180) return answer = 3;
+        if(angle==180) return answer =4;
+    
+    
+    
+    // return answer;
+}
