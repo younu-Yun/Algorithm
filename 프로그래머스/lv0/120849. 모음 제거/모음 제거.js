b83aa7ccadfe78cfd84df48abd3851d4296c5e07
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-03-13 20:00:08 +0900

    [level 0] Title: 모음 제거, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+    var answer = '';
+    
+    for(let i=0; i<my_string.length; i++){
+        if(my_string[i] !== 'a' && my_string[i] !== 'e' && my_string[i] !== 'i' && my_string[i] !== 'o' && my_string[i] !== 'u'){
+            answer += my_string[i];
+        }
+    }
+    return answer;
+}
