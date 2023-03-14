b3f3b3ade7899c828d1450cc34cdf40b48bc3a1a
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-03-14 11:37:43 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = 0;
+    
+    for(let i=1; i<=n; i++){
+        if(i * i === n){
+            answer = 1;
+            break;
+        }else{
+            answer = 2;
+        }
+    }
+    return answer;
+}
