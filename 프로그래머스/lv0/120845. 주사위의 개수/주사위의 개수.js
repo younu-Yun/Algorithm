eca31b506172f8d0ef84a9e8c5d75d47a2166d65
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-03-14 12:22:39 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -0,0 +1,9 @@
+function solution(box, n) {
+    var answer = 0;
+    let boxVolume = parseInt(box[0] / n) * parseInt(box[1] / n) * parseInt(box[2] / n);
+    let diceVolume = Math.pow(n, 3);
+    
+    
+    
+    return boxVolume;
+}
