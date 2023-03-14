09af307fc354edc1245d7b6b0cf66722a78d9991
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-03-14 09:15:44 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -0,0 +1,13 @@
+function solution(sides) {
+    var answer = 0;
+    
+    sides.sort((a,b) => b-a);
+    
+    if(sides[0] < (sides[1] + sides[2])){
+        answer = 1;
+    }else{
+        answer = 2;
+    }
+    
+    return answer;
+}
