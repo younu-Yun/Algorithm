4e66b019544bef909ea41ed0c19a14995c623f2a
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-03-22 18:55:40 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    var answer = 0;
+    
+    let arr = s.split(' ');
+    
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] !== "Z"){
+            answer += Number(arr[i])
+        }else{
+            answer -= Number(arr[i-1]);
+        }
+    }
+    
+    
+    
+    return answer;
+}
