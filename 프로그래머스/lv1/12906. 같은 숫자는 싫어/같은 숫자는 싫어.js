0346f222fe32e1f3bc791319dbcc7508bcf695ad
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-03-31 19:12:48 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 46.23 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,13 +1,7 @@
 function solution(arr)
 {
-    // var answer = [];
-    
-    // const uniq = array => [...new Set(array)];
-    
+
     let answer = arr.filter((item, index) => item !== arr[index + 1])
 
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    console.log('Hello Javascript')
-    
     return answer;
 }
