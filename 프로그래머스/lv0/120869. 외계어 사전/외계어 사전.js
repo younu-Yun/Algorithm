e5af4a05cf28075781bae039ff0191f4af84ef08
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-04-05 20:09:55 +0900

    [level 0] Title: 외계어 사전, Time: 3.47 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -2,8 +2,8 @@ function solution(spell, dic) {
     
     
     for(let i=0; i<spell.length; i++){
-        var dic = dic.filter((item, index) => item.includes(spell[i]))
-        // console.log(dic);
+        dic = dic.filter((item, index) => item.includes(spell[i]))
+        console.log(spell[i], dic);
     }
     
     return dic.length === 0 ? 2 : 1;
