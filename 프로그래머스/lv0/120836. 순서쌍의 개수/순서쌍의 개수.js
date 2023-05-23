645ce6e97fedd4291a3068eb49d6698512001444
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-05-23 13:51:15 +0900

    [level 0] Title: 순서쌍의 개수, Time: 5.96 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,12 +1,10 @@
 function solution(n) {
-    var answer = 0;
-    let arr = [];
+    var answer = [];
     
     for(let i=1; i<=n; i++){
         if(n % i === 0){
-            arr.push([i, n/i])
+            answer.push(i)
         }
     }
-    
-    return arr.length;
+    return answer.length;
 }
