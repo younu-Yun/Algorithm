a7e929f558976b3b219c8ed833838581159344c4
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-05-23 14:13:50 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,11 +1,6 @@
 function solution(my_string, letter) {
     var answer = '';
     
-    for(let i=0; i<my_string.length; i++){
-        if(my_string[i] !== letter){
-            answer += my_string[i];
-        }
-    }
+    return my_string.replaceAll(letter, '')
     
-    return answer;
 }
