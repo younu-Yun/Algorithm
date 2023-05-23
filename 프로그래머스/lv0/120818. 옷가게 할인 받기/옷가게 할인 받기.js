8885224af7a36fd02bbe81df2e8f426a33fc1785
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-05-23 14:17:19 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,12 +1,15 @@
 function solution(price) {
+    var answer = 0;
     
-    if(price >= 500000) {
-        price *= 0.8;
-    } else if(price >= 300000) {
-        price *= 0.9;
-    } else if(price >= 100000) {
-        price *= 0.95;
-    }
-    return Math.floor(price);
-
+        if(price >= 500000){
+            answer = Math.floor(price*0.8)
+        }else if(price >= 300000){
+            answer = Math.floor(price*0.9)
+        }else if(price >= 100000){
+            answer = Math.floor(price*0.95)
+        }else{
+            answer = price
+        }
+    
+    return answer;
 }
