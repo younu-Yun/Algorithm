25dfbed81df271d36ba57c864f8f60d6750d89b7
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-05-26 17:45:00 +0900

    [level 1] Title: 약수의 합, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -2,11 +2,9 @@ function solution(n) {
     var answer = 0;
     
     for(let i=1; i<=n; i++){
-        if(n % i === 0){
-            answer += i
-        }
+        n % i === 0 ? answer += i : answer
     }
     
-    
     return answer;
+    
 }
