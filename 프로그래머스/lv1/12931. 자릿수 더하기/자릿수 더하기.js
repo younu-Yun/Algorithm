fce76940d6a3ede32e8f68bb728e07146953311c
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-05-26 17:56:09 +0900

    [level 1] Title: 자릿수 더하기, Time: 3.05 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,10 +1,17 @@
 function solution(n)
 {
-    var arr = String(n).split('');
-    let answer = arr.reduce((acc, cur, idx) => {
-        return Number(acc) + Number(cur);
-    }, 0)
+    var answer = 0;
+    const arr = String(n).split('');
+    
     
 
-    return answer;
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log(arr)
+    
+    if(arr.length === 1){
+        return n
+    }else{
+        return arr.reduce((prev, cur) => Number(prev) + Number(cur));    
+    }
+    
 }
