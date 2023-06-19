cf78e4cb4f3d47602993061e5914d1c5a0a3caf0
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-06-20 01:16:29 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,9 +1,14 @@
 function solution(arr) {
-    var answer = [];
-    let min = [...arr].sort((a, b) => a - b)[0];
-    let min_idx = arr.indexOf(min)
     
-    arr.splice(min_idx, 1)
+    if(arr.length === 1) {
+        return [-1];
+    }
     
-    return arr.length === 0 ? [-1] : arr;
+    let min = [...arr].sort((a,b) => a - b)[0];
+    
+    
+    const answer = arr.filter(item => item !== min)
+    
+    return answer
+    
 }
